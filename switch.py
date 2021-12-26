95cf61fc916c4b8b8d7f197bf447fdc3a1bb9a9a
Sergey ANOKHIN <star0s@mail.ru>
2021-12-26 20:17:03 +0100

    switch fix.

--- switch.py
+++ switch.py
@@ -36,39 +36,30 @@ class CameraArchiverEnabler(RestoreEntity, ToggleEntity):
         self._device_name = config[CONF_NAME]
         self._attr_name = self._device_name + " Enabler"
         self._attr_available = True
+        self._attr_device_class = DEVICE_CLASS_SWITCH
 
-    async def update(self):
-        self._attr_state = STATE_ON if self._attr_is_on else STATE_OFF
-        self.schedule_update_ha_state()
-        self.coordinator.data[CONF_ENABLE] = self._attr_is_on
-        await self.coordinator.async_request_refresh()
 
     async def async_turn_on(self, **kwargs):
         """Turn the light on."""
-        self._attr_is_on = True
-        await self.update()
+        self._attr_state = STATE_ON
 
     async def async_turn_off(self, **kwargs):
         """Turn the entity off."""
-        self._attr_is_on = False
-        await self.update()
+        self._attr_state = STATE_OFF
+
+    async def async_update(self, **kwargs):
+        self._attr_is_on = self._attr_state == STATE_ON
+        # self.schedule_update_ha_state()
+        self.coordinator.data[CONF_ENABLE] = self._attr_is_on
+        await self.coordinator.async_request_refresh()
 
     async def async_added_to_hass(self):
         last_state = await self.async_get_last_state()
-        _LOGGER.info(f"#{self._attr_name}# call async_get_last_state STATE={self._attr_state}")
+        _LOGGER.debug(f"#{self._attr_name}# call async_get_last_state STATE={self._attr_state}")
         if last_state and last_state.state:
             self._attr_state = last_state.state
-            _LOGGER.info(f"#{self._attr_name}# NEW_STATE={self._attr_state}")
-
-    # @property
-    # def device_class(self):
-    #     """Return the class of this device, from component DEVICE_CLASSES."""
-    #     return DEVICE_CLASS_SWITCH
-
-    # @property
-    # def available(self) -> bool:
-    #     super().available
-    #     return True
+            _LOGGER.debug(f"#{self._attr_name}# NEW_STATE={self._attr_state}")
+            await self.async_update()
 
     # @property
     # def should_poll(self) -> bool:
